9b69fe9ad53215af1a5d55595997f7b61f07d06e
Mari Vaypane <avemari98@gmail.com>
2023-09-09 18:04:03 +0300

    add form search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -5,6 +5,11 @@ import { Wrapper } from './Search.styles'
 export default function Search() {
   return (
     <Wrapper>
+        <form className="search-block">
+            <h1>Search for books</h1>
+            <input type="text" placeholder="Type book name here" />
+            <button>Search 🔍︎</button>
+        </form>
 
     </Wrapper>
   )
