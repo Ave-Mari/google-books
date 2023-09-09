ab9b9158851d48a7971281dabb209a1d3f1bd1bc
Mari Vaypane <avemari98@gmail.com>
2023-09-09 18:04:18 +0300

    add background image and styles to form

--- src/components/Search/Search.styles.js
+++ src/components/Search/Search.styles.js
@@ -1,5 +1,27 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.header`   
+    background: no-repeat url("./src/components/Search/books-background.jpg");
+    background-size: cover;
+    display: flex;    
+    justify-content: center;
+    padding-top: 60px;
+    
+    h1 {
+        margin-top: 0;
+        color: #fff;
+        display: block;
+    }
+
+    form {        
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 30%;
+
+        button {
+            width: 20%;
+        }
+    }
 
 `
